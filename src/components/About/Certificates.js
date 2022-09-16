76b5cf20b49d02c65edb4c524006b056ac195cff
codergalib <coderboygalif@gmail.com>
2022-09-16 22:17:46 +0600

    commited

--- src/components/About/Certificates.js
+++ src/components/About/Certificates.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CertificateItem from "./CertificateItem";
 
 const Certificates = () => {
+  const [certifications, setCertifiactons] = useState([]);
+  useEffect(() => {
+    fetch("/certifications.json")
+      .then((res) => res.json())
+      .then((data) => setCertifiactons(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="text-white py-12">
       <h2 className="text-3xl font-bold uppercase text-center text-border">
         Certificates
       </h2>
-      <div className="grid grid-cols-4">
-        
-      </div>
+      <div className="grid grid-cols-4"></div>
       <div>
         <CertificateItem />
       </div>
